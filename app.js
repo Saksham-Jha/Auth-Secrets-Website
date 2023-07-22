c4502a21e6408a19caecd36f814f71ce1898462d
Saksham Jha <saksham.sambhav99@gmail.com>
2023-07-23 01:35:34 +0530

    new commit

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ passport.serializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://auth-secrets-website.onrender.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     // console.log(profile);
